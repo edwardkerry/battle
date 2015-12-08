041ba11596e3951e8774af58a2d9fa3cc41f183b
Chris Wynne <chris.wynndow@gmail.com>
2015-12-08 16:21:50 +0000

    Adds solution to 19

--- app.rb
+++ app.rb
@@ -1,15 +1,24 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
+  get '/play' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
+    erb(:play)
+  end
+
   post '/names' do
     p params
-  @player1 = params[:player1]
-  @player2 = params[:player2]
-  erb(:names_page)
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+    redirect '/play'
   end
 
   # start the server if ruby file executed directly
--- spec/feature/feature_spec.rb
+++ spec/feature/feature_spec.rb
@@ -5,7 +5,7 @@ Capybara.default_driver= :selenium
 
 describe 'feature test' do
   it 'should display the players name' do
-    visit('localhost:9393')
+    visit('localhost:9292')
     fill_in 'player1', with: 'Tom'
     fill_in 'player2', with: 'Chris'
     click_button('Submit')
