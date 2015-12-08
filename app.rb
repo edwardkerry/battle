327bbed25c95d6c9dbc64f1deafd0ff54177c809
tmgree <tmgree@outlook.com>
2015-12-08 15:32:37 +0000

    still 18

--- app.rb
+++ app.rb
@@ -5,6 +5,13 @@ class Battle < Sinatra::Base
     erb(:index)
   end
 
+  post '/names' do
+    p params
+  @player1 = params[:player1]
+  @player2 = params[:player2]
+  erb(:names_page)
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,13 +1,14 @@
 require 'capybara'
 require 'selenium-webdriver'
 include Capybara::DSL
+Capybara.default_driver= :selenium
 session = Capybara::Session.new :selenium
 
 describe 'feature test' do
   it 'should display the players name' do
-    visit(localhost:4567)
-    fill_in('PLayer_1', with: 'Tom')
-    fill_in('PLayer_2', with: 'Chris')
+    visit('localhost:9393')
+    fill_in 'player_1', with: 'Tom'
+    fill_in 'player_2', with: 'Chris'
     click_button('Submit')
     expect(page).to have_content 'Tom Vs. Chris'
   end
