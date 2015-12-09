38520c82c35be2237a044a5575cafc67429de5ea
Chris Wynne <chris.wynndow@gmail.com>
2015-12-09 13:57:29 +0000

    Refactor for the law of demeter

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,7 @@
 class Game
 
+# WORK OUT HOW TO USE FORWARDABLE AT SOME POINT INSTEAD OF THE NAME METHODS.
+
   attr_reader :player1, :player2
 
   def initialize(player1, player2)
@@ -10,4 +12,17 @@ class Game
   def hug(player)
     player.reduce_hug_points
   end
+
+  def player1_name
+    player1.name
+  end
+
+  def player2_name
+    player2.name
+  end
+
+  def player2_hug_points
+    @player2.hug_points
+  end
+  
 end
